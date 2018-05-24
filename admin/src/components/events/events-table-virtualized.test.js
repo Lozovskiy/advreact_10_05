343423aab3175b267d8f71724d3a33e91dd0fceb
Mariia Ilyukhina <aleksandre.lozovskiy@gmail.com>
2018-05-25 01:23:47 +0300

    add tests

---
+++ admin/src/components/events/events-table-virtualized.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import eventsList from '../../mocks/conferences'
+
+import { EventsTable } from './events-table-virtualized'
+
+const events = eventsList.map((event) => ({ ...event, uid: Math.random() }))
+
+describe('EventLazyTable', () => {
+  it('should render 3 rows', () => {
+    const table = mount(<EventsTable events={events.slice(0, 3)} />)
+
+    expect(table.find('.ReactVirtualized__Table__row').length).toBe(3)
+  })
+
+  it('should render 18 rows', () => {
+    const table = mount(<EventsTable events={events} />)
+
+    expect(table.find('.ReactVirtualized__Table__row').length).toBe(18)
+  })
+})
